936c0305c21ada2210ddd544768ac395da800b7e
pierre <pierre.paquin1@etu.univ-lorraine.fr>
2021-04-14 08:40:50 +0200

    Initial commit

---
+++ src/test/java/com/example/openshiftlombard/OpenshiftLombardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openshiftlombard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftLombardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
